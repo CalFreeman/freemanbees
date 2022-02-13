02838c1e5ada74d3c76fec26e5b20a90934bf486
caleb <caleb@localhost.localdomain>
2022-02-13 02:51:52 -0500

    escape '

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -2,13 +2,13 @@ const Blog = () => {
     return (
         <div>
             <h1>About</h1>
-            <p>I don't even care what I think, go away</p>
-            <p>I don't even care what I think, go away</p>
-            <p>I don't even care what I think, go away</p>
-            <p>I don't even care what I think, go away</p>
-            <p>I don't even care what I think, go away</p>
-            <p>I don't even care what I think, go away</p>
-            <p>I don't even care what I think, go away</p>
+            <p>I dont even care what I think, go away</p>
+            <p>I dont even care what I think, go away</p>
+            <p>I dont even care what I think, go away</p>
+            <p>I dont even care what I think, go away</p>
+            <p>I dont even care what I think, go away</p>
+            <p>I dont even care what I think, go away</p>
+            <p>I dont even care what I think, go away</p>
         </div>
     );
 }
