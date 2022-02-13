43d67f5c4fd17bd1d19c0bd184432e15dbf73b98
caleb <caleb@localhost.localdomain>
2022-02-13 02:13:10 -0500

    centered image

--- components/home/Banner.tsx
+++ components/home/Banner.tsx
@@ -4,7 +4,11 @@ import { APP_NAME } from "../../lib/utils/constant";
 
 const Banner = () => (
   <div className="banner-container">
-        <img src="images/60028.jpg" style={{width: "100%", height: "100%", float: "left"}}/>
+        <img src="images/60028.jpg" style={{width: "100%", height: "100%",
+        marginLeft: "auto",
+        marginRight: "auto",
+        maxWidth: "400px",
+        maxHeight: "400px" }}/>
 
     <div className="banner">
         <h1 className="logo-font">{APP_NAME.toUpperCase()}</h1>
