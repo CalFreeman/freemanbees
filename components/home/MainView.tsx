d35779107f5d7f3590fd7eed015efd5815244c75
calfreeman <ctfreeman1s@gmail.com>
2022-02-16 21:10:50 -0500

    image added

--- components/home/MainView.tsx
+++ components/home/MainView.tsx
@@ -4,6 +4,8 @@ import React from "react";
 
 const MainView = () => (
   <div className="col-md-9">
+    <img className="mainImg" src="images/dustin-humes-Xd-DRy9jJMc-unsplash(1).jpg"/>
+
     <div className="feed-toggle">
     </div>
   </div>
