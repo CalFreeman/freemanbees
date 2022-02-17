b62ad4f7515894c27a7a35597e9787871fc7968f
CalFreeman <ctfreeman1s@gmail.com>
2022-02-17 00:21:38 -0500

    hamburger flowing

--- components/common/Hamburger.tsx
+++ components/common/Hamburger.tsx
@@ -12,34 +12,34 @@ const Hamburger = () => {
   return (
     <nav className="navbar">
       <div className="container"></div>
-    <button className="hamburger " onClick={handleToggle}>{navbarOpen ? "Close" : "Open"}
+    <button className="hamburger " onClick={handleToggle}>{navbarOpen ? "" : "Menu"}
     <span></span>
     <span></span>
     <span></span>
     </button>
         <ul className={`navbar-menu-hamburger ${navbarOpen ? " showMenu" : ""}`}>
 
-          <li className="nav-item">
+          <li className="nav-itemHamb">
             <NavLink href="/about" as="/about">
               <span>About</span>
             </NavLink>
           </li>
-          <li className="nav-item ">
+          <li className="nav-itemHamb ">
             <NavLink href="/blog" as="/blog">
               <span>Blog</span>
             </NavLink>
           </li>
-          <li className="nav-item">
+          <li className="nav-itemHamb">
             <NavLink href="/bees" as="/bees">
               <span>Bees</span>
             </NavLink>
           </li>
-          <li className="nav-item">
+          <li className="nav-itemHamb">
             <NavLink href="/honey" as="/honey">
               <span>Honey</span>
             </NavLink>
           </li>
-          <li className="nav-item">
+          <li className="nav-itemHamb">
             <NavLink href="/" as="/">
               <span>Home</span>
             </NavLink>
